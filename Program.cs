97f9452b0281bc6c554b847ebc327f413c47b55b
Raul Crisostomo Gomes <raul.cgomes92@gmail.com>
2021-12-21 00:44:51 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalInnovatioOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerdevezes = 5;
+
+            for(int i=0; i<numerdevezes; i++){
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
